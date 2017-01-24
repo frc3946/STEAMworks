757f8d13e703b44171c37f21c045cd321ac13b09
brilliance1 <tclark79301@gmail.com>
2017-01-23 18:19:21 -0600

    testing

--- src/org/usfirst/frc/team3946/robot/OI.java
+++ src/org/usfirst/frc/team3946/robot/OI.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team3946.robot;
 
 import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.Joystick;
 
 import org.usfirst.frc.team3946.robot.commands.ExampleCommand;
 
@@ -9,14 +11,17 @@ import org.usfirst.frc.team3946.robot.commands.ExampleCommand;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+	
+	Joystick driveController = new Joystick(1);
+	
 	//// CREATING BUTTONS
 	// One type of button is a joystick button which is any button on a
 	//// joystick.
 	// You create one by telling it which joystick it's on and which button
 	// number it is.
 	// Joystick stick = new Joystick(port);
-	// Button button = new JoystickButton(stick, buttonNumber);
-
+	Button leftTrain = new JoystickButton(driveController, 1);
+	Button rightTrain = new JoystickButton(driveController, 2);
 	// There are a few additional built in buttons you can use. Additionally,
 	// by subclassing Button you can create custom triggers and bind those to
 	// commands the same as any other Button.
--- src/org/usfirst/frc/team3946/robot/RobotMap.java
+++ src/org/usfirst/frc/team3946/robot/RobotMap.java
@@ -7,10 +7,10 @@ package org.usfirst.frc.team3946.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
+	public static int fRightDriveTalon = 1;
+	public static int fLeftDriveTalon = 2;
+	public static int bRightDriveTalon = 3;
+	public static int bLeftDriveTalon = 4;
 
 	// If you are using multiple modules, make sure to define both the port
 	// number and the module. For example you with a rangefinder:
