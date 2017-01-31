cc92df8e550b0c5b74919a6e6ced7d946feef1f0
Chris Eskine <chriseskine3@gmail.com>
2017-01-30 19:03:27 -0600

    added winch

---
+++ src/org/usfirst/frc/team3946/robot/subsystems/ClimbMotor.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team3946.robot.subsystems;
+
+import org.usfirst.frc.team3946.robot.Robot;
+import org.usfirst.frc.team3946.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class ClimbMotor extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	public CANTalon winchMotor = new CANTalon(RobotMap.winchTalon);
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    public void forward() {
+    	winchMotor.set(CANTalon.Value.kFoward);
+    	
+    }
+}
+
