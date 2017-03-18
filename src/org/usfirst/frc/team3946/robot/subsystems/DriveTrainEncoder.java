73dfb988ea4f1d2ecc960e3388ad9b8d7618e52b
brilliance1 <noodles.spaz@gmail.com>
2017-03-18 10:13:09 -0500

    pushing some code

--- src/org/usfirst/frc/team3946/robot/subsystems/DriveTrainEncoder.java
+++ src/org/usfirst/frc/team3946/robot/subsystems/DriveTrainEncoder.java
@@ -38,6 +38,8 @@ public class DriveTrainEncoder extends Subsystem {
     	 
     		double rawData = RobotMap.fRight.getEncPosition();
         	return - rawData/ticksCal;
+        	
+        	
      }
      
      public double getLeftDistance()
